25958d3ea3bfdbc203932746384574bb6e5d341a
Alex Lewis <alexlewis374@gmail.com>
2020-06-22 16:40:05 +0100

    Add test to check travis build

---
+++ src/app.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import App from './app.js'
+import { shallow } from 'enzyme'
+
+describe('Renders', () => {
+  it('hello world', () => {
+    const wrapper = shallow(<App />)
+
+    expect(wrapper).toHaveText('Hello World')
+  })
+})
