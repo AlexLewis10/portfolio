f90f5ca1b84598c382b5d5ae4dc532ee88d3208a
Alex Lewis <alexlewis374@gmail.com>
2020-06-23 15:37:32 +0100

    G: Home, clicking CV button displays the CV

--- src/components/app.js
+++ src/components/app.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react'
 import profilepic from '../img/profilepic.jpg'
 
 export class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      displayCv: false
+    }
+  }
+
+  showCv = () => {
+    this.setState({ displayCv: true })
+  }
+
   render () {
-    return (
-      <div id="mid_div">
-        <img src={profilepic} alt="Alex Lewis"></img>
-        <h2>Hi, I&#39;m Alex Lewis</h2>
-      </div>
-    )
+    if (this.state.displayCv === false) {
+      return (
+        <div>
+          <div id="mid_div">
+            <img src={profilepic} alt="Alex Lewis"></img>
+            <h2>Hi, I&#39;m Alex Lewis</h2>
+          </div>
+          <div>
+            <button id='cv' className='btn' onClick={this.showCv}>CV</button>
+          </div>
+        </div>
+      )
+    }
+    if (this.state.displayCv) {
+      return (
+        <div><h3>CV</h3></div>
+      )
+    }
   }
 }
 
--- src/components/app.test.js
+++ src/components/app.test.js
@@ -4,15 +4,24 @@ import profilepic from '../img/profilepic.jpg'
 import { shallow } from 'enzyme'
 
 describe('Home', () => {
-  it('Hi, I\'m Alex Lewis renders', () => {
+  it('hi, I\'m Alex Lewis renders', () => {
     const wrapper = shallow(<App />)
 
     expect(wrapper).toContainReact(<h2>Hi, I&#39;m Alex Lewis</h2>)
   })
 
-  it('Has profilepic image', () => {
+  it('has profilepic image', () => {
     const wrapper = shallow(<App />)
 
     expect(wrapper).toContainReact(<img src={profilepic} alt="Alex Lewis"></img>)
+  })
+
+  it('clicking CV button displays the CV', () => {
+    const wrapper = shallow(<App />)
+
+    const button = wrapper.find('#cv')
+    button.simulate('click')
+
+    expect(wrapper).toContainReact(<div><h3>CV</h3></div>)
   })
 })
