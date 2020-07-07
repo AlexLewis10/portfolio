4094727ff2f6fda855ae638d3709e97a0973c359
Alex Lewis <alexlewis374@gmail.com>
2020-07-07 15:50:17 +0100

    Add grid to nav style

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,15 +3,13 @@ import { Link } from 'react-router-dom'
 
 function Nav () {
   return (
-    <nav>   
-    <ul>   
+    <nav id='mainNav'>   
       <a className='links' href='https://github.com/AlexLewis10/CV/raw/master/AlexLewisCV.pdf' download>CV</a>
       <a className='links' href='https://github.com/AlexLewis10/CV'>Github</a>
       <Link className='links' to='/projects'>Projects</Link>
       <a className='links' href='https://www.linkedin.com/in/alex-lewis-022761a8/'>LinkedIn</a>
       <a className='links' href='https://medium.com/@alexlewis374'>Medium</a>
       <a className='links' href='https://www.codewars.com/users/ALJALE'>CodeWars</a>
-    </ul>
     </nav>
   )
 }
